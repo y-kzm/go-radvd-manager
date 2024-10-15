cfb241df5d806c427a76c968ec3b1055382d48a0
y-kzm <kzm-yokoo@hiroshima-u.ac.jp>
2024-10-15 14:45:40 +0900

    add template

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -4,8 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 
-	//"github.com/y-kzm/go-radvd-manager/internal/client"
+	"github.com/y-kzm/go-radvd-manager/internal/client"
 	"github.com/y-kzm/go-radvd-manager/internal/config"
 )
 
@@ -14,71 +15,73 @@ const (
 )
 
 func main() {
-	//hostFlag := flag.String("h", "localhost", "Hostname")
-	//methodFlag := flag.String("X", "", "HTTP Method (POST, GET, DELETE)")
-	fileFlag := flag.String("f", "", "Config file")
-	idFlag := flag.Int("i", -1, "Instance ID")
+	methodFlag := flag.String("X", "", "HTTP Method (POST, GET, DELETE)")
+	fileFlag := flag.String("f", "", "Policy config file")
 	flag.Parse()
 
-	//client := client.NewClient(fmt.Sprintf("http://%s:%d", *hostFlag, port))
-	if *idFlag == -1 {
-		log.Fatal("Usage: -i <id> is required")
-	}
-
-	// 設定ファイルを読み込む
+	// read policy config file
 	cfg, err := config.LoadPolicyConfig(*fileFlag)
 	if err != nil {
 		log.Fatalf("Failed to load config: %v", err)
 	}
 
-	// パースされたルールとポリシーを表示する
-	fmt.Println("Rules:")
+	// print config file content
+	fmt.Println("--- Rules: ---")
 	for _, rule := range cfg.Rules {
 		fmt.Printf("ID: %d, Type: %s, FQDN: %s, Prefix: %s, Nexthop: %s\n",
 			rule.ID, rule.Type, rule.FQDNs, rule.Prefixes, rule.Nexthop)
 	}
-
-	fmt.Println("\nPolicies:")
+	fmt.Println("--- Policies: ---")
 	for _, policy := range cfg.Policies {
 		fmt.Printf("ID: %d, Description: %s, Rules: %v, Clients: %v\n",
 			policy.ID, policy.Description, policy.Rules, policy.Clients)
 	}
 
-	/*
+	// generate radvd config files from policy config
+	radvdConfigs, err := cfg.GenerateRadvdConfigFile()
+	if err != nil {
+		log.Fatalf("Failed to generate radvd config files: %v", err)
+	}
+	fmt.Println("\nRadvd config files generated successfully")
 
-		switch *methodFlag {
-		case "POST":
-			if *fileFlag == "" {
-				log.Fatal("Usage: -X POST -f <config_file> -i <id> -h <hostname>")
-			}
-			config, err := os.ReadFile(*fileFlag)
+	// create client
+	var clients []*client.RadvdManagerClient
+	severs := client.GetSeverList(radvdConfigs)
+	for _, server := range severs {
+		client := client.NewClient(fmt.Sprintf("http://[%s]:%d", server, port), server, port)
+		clients = append(clients, client)
+	}
+
+	if *fileFlag == "" || *methodFlag == "" {
+		log.Fatal("Usage: -X POST -f <config_file>")
+		return
+	}
+
+	// send request
+	switch *methodFlag {
+	case "POST":
+		for _, radvdConfig := range radvdConfigs {
+			config, err := os.ReadFile(radvdConfig.FilePath)
 			if err != nil {
 				log.Fatalf("Failed to read config file: %v", err)
 			}
-			if err := client.Create(*idFlag, string(config)); err != nil {
-				log.Fatalf("Failed to create radvd instance: %v", err)
+			for _, client := range clients {
+				if client.Server == radvdConfig.Rule.Nexthop {
+					if err := client.Create(radvdConfig.Rule.ID, string(config)); err != nil {
+						log.Fatalf("Failed to create radvd instance: %v", err)
+					}
+				}
 			}
-			fmt.Println("radvd instance created successfully")
-
-		case "GET":
-			config, err := client.Get(*idFlag)
-			if err != nil {
-				log.Fatalf("Failed to get radvd instance: %v", err)
-			}
-			var data map[string]string
-			if err := json.Unmarshal([]byte(config), &data); err != nil {
-				log.Fatalf("Failed to unmarshal config: %v", err)
-			}
-			fmt.Println(data["config"])
-
-		case "DELETE":
-			if err := client.Delete(*idFlag); err != nil {
-				log.Fatalf("Failed to delete radvd instance: %v", err)
-			}
-			fmt.Println("radvd instance deleted successfully")
-
-		default:
-			log.Fatalf("Unknown or unsupported method: %s. Use POST, GET, or DELETE", *methodFlag)
+			fmt.Printf("radvd instance created successfully (%d)", radvdConfig.Rule.ID)
 		}
-	*/
+		fmt.Println("radvd instances created successfully (all)")
+	case "GET":
+		// not implemented
+
+	case "DELETE":
+		// not implemented
+
+	default:
+		log.Fatalf("Unknown or unsupported method: %s. Use POST, GET, or DELETE", *methodFlag)
+	}
 }
